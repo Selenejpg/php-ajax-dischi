59be616ea15c30e219b202b9397366d1715cb472
noemipintaldi <noemipintaldi@gmail.com>
2022-05-09 19:28:07 +0200

    struttura iniziale

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi php</title>
+</head>
+<body>
+    
+    <div id="root">
+
+    </div>
+
+<!-- Vue cdn -->
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+<!-- Axios cdn -->
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<!-- Main js -->
+<script src="./JS/main.js"></script>
+</body>
+</html>
